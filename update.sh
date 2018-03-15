47606a2bd1231318da00fa872ce28e5f7925da57
iovxw <iovxw@outlook.com>
2018-03-15 19:27:49 +0800

    Only push data.json

--- update.sh
+++ update.sh
@@ -4,11 +4,17 @@ set -ex
 SOURCE="https://api.wynncraft.com/public_api.php?action=itemDB&category=all"
 OUTPUT="data.json"
 
-curl -o $OUTPUT $SOURCE
 git config --global credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 git config --global user.email "deploy@travis-ci.org"
 git config --global user.name "Deployment Bot (from Travis CI)"
+
+git remote set-branches origin '*'
+git fetch origin data
+git checkout data
+
+curl -o $OUTPUT $SOURCE
+
 git add $OUTPUT
 git commit -m "Auto update"
-git push origin HEAD:refs/heads/data
+git push origin data
