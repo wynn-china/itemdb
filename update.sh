eb73019657e1b9e3a2f063ffacb351376fef4e2f
iovxw <iovxw@outlook.com>
2018-03-15 18:55:52 +0800

    Fix branch prefix

--- update.sh
+++ update.sh
@@ -11,4 +11,4 @@ git config --global user.email "deploy@travis-ci.org"
 git config --global user.name "Deployment Bot (from Travis CI)"
 git add $OUTPUT
 git commit -m "Auto update"
-git push origin HEAD:data
+git push origin HEAD:refs/heads/data
