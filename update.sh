53ea4295473a414eee3768a11bdd7461af1070a3
iovxw <iovxw@outlook.com>
2018-03-15 18:52:09 +0800

    Push to data branch

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -ex
 
-curl -o data.json "https://api.wynncraft.com/public_api.php?action=itemDB&category=all"
+SOURCE="https://api.wynncraft.com/public_api.php?action=itemDB&category=all"
+OUTPUT="data.json"
+
+curl -o $OUTPUT $SOURCE
 git config --global credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 git config --global user.email "deploy@travis-ci.org"
 git config --global user.name "Deployment Bot (from Travis CI)"
-git commit -am "Auto update"
-git push
+git add $OUTPUT
+git commit -m "Auto update"
+git push origin HEAD:data
